aef31e2e35d76c7ee55ea9e7a462ad3cbf59b13b
Suchita Doshi <sudoshi@linkedin.com>
2019-11-11 16:21:15 -0800

    add item detail as an in repo addon

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({});
+export default Route.extend({
+  model() {
+    return [...Array(4)].map((_, i) => {
+      return {
+        itemName: `Some Rando Item #${i + 1}`,
+        className: `some-rando-item-${i + 1}`,
+      };
+    });
+  },
+});
